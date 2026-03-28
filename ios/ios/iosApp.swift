93936c70067696d5e9ff5505a4dd8ba897295391
Daccho <dacchotech@gmail.com>
2026-03-28 22:36:54 +0900

    client init

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Kotaro Ueda on 2026/03/28.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
